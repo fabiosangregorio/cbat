183164762f2f0f4345962af1f35705aa56c7d9b1
Fabio Sangregorio <fabio.sangregorio@gmail.com>
2019-02-01 10:32:39 +0100

    Prepare main.py for running on bender

--- progetto-tesi/main.py
+++ progetto-tesi/main.py
@@ -1,42 +1,43 @@
+#!/usr/bin/env python3
+
 import scraper
 import program_extractor
 from person import Person
 
-# # url con due righe
-# # url = "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=10040&copyownerid=12184"
+if __name__ == "__main__":
+    # # url con due righe
+    # # url = "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=10040&copyownerid=12184"
 
-# # url con una riga
-# url = "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=52345&copyownerid=75434"
-# html = scraper.get_wikiCFP(url)
+    # # url con una riga
+    # url = "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=52345&copyownerid=75434"
+    # html = scraper.get_wikiCFP(url)
 
-# program_committee = program_extractor.extract_program_committee(html)
- 
+    # program_committee = program_extractor.extract_program_committee(html)
+    
 
-# program_committee = [Person('Kai Chen', 'Chinese Academy of Sciences, China')]
+    # program_committee = [Person('Kai Chen', 'Chinese Academy of Sciences, China')]
+    with open('progetto-tesi/program_test.txt', 'r') as f:
+        data = f.read()
 
-with open('progetto-tesi/program_test.txt', 'r') as f:
-    data = f.read()
+    program_committee = [Person(p.split('#')[0], p.split('#')[1]) for p in data.splitlines()]
+    people = list()
 
-program_committee = [Person(p.split('#')[0], p.split('#')[1]) for p in data.splitlines()]
-people = list()
-
-for person in program_committee:
-    # scraper.search_dblp(person)
-    people.append({
-        "person_name": person.name,
-        "person_affiliation": person.affiliation,
-        "result": scraper.search_dblp(person)['result'] if scraper.search_dblp(person)['status'] != 'error' else None
-    })
-
-print('xd')
-
-# with open('progetto-tesi/results.txt', 'w') as f:
-#     for person in people:
-#         f.write(f'\n\n================================\nname: {person["person_name"]}, affiliation: {person["person_affiliation"]}\n================================\n')
-#         for ratio in person['results']:
-#             ratio["results"].sort(reverse=True, key=lambda x: x[1])
-#             f.write('\n\n' + ratio["name"] + '\n')
-#             f.write("\n".join([": ".join(reversed([str(r) for r in list(res)])) for res in ratio["results"]]))
+    for person in program_committee:
+        # scraper.search_dblp(person)
+        people.append({
+            "person_name": person.name,
+            "person_affiliation": person.affiliation,
+            "result": scraper.search_dblp(person)['result'] if scraper.search_dblp(person)['status'] != 'error' else None
+        })
 
 
-    # papers = scraper.get_papers_from_dblp(search_result.name_url)
+    # with open('progetto-tesi/results.txt', 'w') as f:
+    #     for person in people:
+    #         f.write(f'\n\n================================\nname: {person["person_name"]}, affiliation: {person["person_affiliation"]}\n================================\n')
+    #         for ratio in person['results']:
+    #             ratio["results"].sort(reverse=True, key=lambda x: x[1])
+    #             f.write('\n\n' + ratio["name"] + '\n')
+    #             f.write("\n".join([": ".join(reversed([str(r) for r in list(res)])) for res in ratio["results"]]))
+
+
+        # papers = scraper.get_papers_from_dblp(search_result.name_url)
